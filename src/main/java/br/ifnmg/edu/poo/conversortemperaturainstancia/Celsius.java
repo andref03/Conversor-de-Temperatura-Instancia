f28d770b0d5737e67fcdb099e1a45bbbd76d2f46
André Felipe <140921456+andref03@users.noreply.github.com>
2024-06-03 17:35:15 -0300

    detalhamento em Celsius.java

--- src/main/java/br/ifnmg/edu/poo/conversortemperaturainstancia/Celsius.java
+++ src/main/java/br/ifnmg/edu/poo/conversortemperaturainstancia/Celsius.java
@@ -1,7 +1,7 @@
 package br.ifnmg.edu.poo.conversortemperaturainstancia;
 
 /**
- *
+ * Classe que contém métodos específicos a partir da temperatura Celsius.
  * @author André (@andref03)
  */
 public class Celsius {
